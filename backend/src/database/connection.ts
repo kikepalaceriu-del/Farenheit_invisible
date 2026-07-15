f054b792437e6527b50cacc6f5ae79ab23bb0422
Enrique <kikepalaceriu@gmail.com>
2026-07-14 18:23:00 -0600

    Restore backend configuration modules after security cleanup

---
+++ backend/src/database/connection.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+import { env } from "../config/env";
+
+const pool = new Pool({
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  database: env.DB_NAME,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+});
+
+export default pool;
+
